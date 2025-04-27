e4955a1ced09ed4dc22f7181b87476decdf0a10b
Petya Yaneva <petyayaneva04@gmail.com>
2025-04-27 22:21:50 +0300

    Delete project-management-system directory

--- project-management-system/src/main/java/com/project_management_system/config/JWTConstant.java
+++
@@ -1,6 +0,0 @@
-package com.project_management_system.config;
-
-public class JWTConstant {
-    public static final String SECRET_KEY="sjjsjsjsjjjjjjjjjjjjjjjjjjsjsjjsjsjjsjjsjsjkkwkw";
-    public static final String JWT_HEADER="Authorization";
-}
--- project-management-system/src/main/java/com/project_management_system/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project_management_system.repository;
-
-import com.project_management_system.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByEmail(String email);
-}
